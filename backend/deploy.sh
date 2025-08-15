b015156633e1f7c46f59c2978e659b9eabff1dde
std-ext-019-01 <std-ext-019-01>
2025-08-15 13:13:15 +0300

    vault

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -8,5 +8,10 @@ sudo docker run --rm -d --name sausage-backend \
      --env SPRING_DATASOURCE_USERNAME="${SPRING_DATASOURCE_USERNAME}" \
      --env SPRING_DATASOURCE_PASSWORD="${SPRING_DATASOURCE_PASSWORD}" \
      --env SPRING_DATA_MONGODB_URI="${SPRING_DATA_MONGODB_URI}" \
+     --env SPRING_CLOUD_VAULT_TOKEN="hvs.CAESIJRyaeMPqZPYzADF1qh1dk6TQsKx2IFzjpq3bmYET49NGh4KHGh2cy40UXE1Q05aY2lqRE9SZ2tmRTVNSm9oVW0" \
+     --env SPRING_CLOUD_VAULT_SCHEME="http" \
+     --env SPRING_CLOUD_VAULT_HOST="std-ext-019-01.praktikum-services.tech" \
+     --env SPRING_CLOUD_VAULT_KV_ENABLED="true" \
+     --env SPRING_CONFIG_IMPORT="vault://secret/sausage-store/" \
      --network=sausage_network \
      "${CI_REGISTRY_IMAGE}"/sausage-backend:${VERSION}

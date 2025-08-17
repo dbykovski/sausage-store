2369cf6aec8506dbd166740bd5b198cddcf84cbc
std-ext-019-01 <std-ext-019-01>
2025-08-17 20:34:39 +0300

    blue-green

--- frontend/deploy-frontend.sh
+++ frontend/deploy-frontend.sh
@@ -3,4 +3,4 @@ set -xe
 sudo docker login -u ${CI_REGISTRY_USER} -p ${CI_REGISTRY_PASSWORD} ${CI_REGISTRY}
 sudo docker pull ${CI_REGISTRY_IMAGE}/sausage-frontend:${VERSION}
 docker compose -f ~/docker-compose-frontend.yml up -d --force-recreate
-timeout 10 bash -c 'until docker inspect -f {{.State.Health.Status}} sausage-backend-blue | grep healthy || docker inspect -f {{.State.Health.Status}} sausage-backend-green | grep healthy; do sleep 5; done' || exit 1
+timeout 300 bash -c 'until docker inspect -f {{.State.Health.Status}} sausage-backend-blue | grep healthy || docker inspect -f {{.State.Health.Status}} sausage-backend-green | grep healthy; do sleep 5; done' || exit 1

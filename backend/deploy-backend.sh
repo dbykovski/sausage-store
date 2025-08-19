022388c007515827471929c623b2a8ee95cf039e
std-ext-019-01 <std-ext-019-01>
2025-08-19 17:19:15 +0300

    blue-green

--- backend/deploy-backend.sh
+++ backend/deploy-backend.sh
@@ -1,10 +1,13 @@
 #! /bin/bash
 set -xe
 
-docker context rm remote || true
-docker context create remote --description 'remote ssh' --docker host=ssh://student@std-ext-019-01.praktikum-services.tech
+if ! docker context inspect remote >/dev/null 2>&1; then
+  docker context create remote \
+    --description 'remote ssh' \
+    --docker host=ssh://student@std-ext-019-01.praktikum-services.tech
+fi
 
 sudo docker login -u ${CI_REGISTRY_USER} -p ${CI_REGISTRY_PASSWORD} ${CI_REGISTRY}
 sudo docker pull ${CI_REGISTRY_IMAGE}/sausage-backend:${VERSION}
 
-docker --context remote compose -f ../docker-compose.yml up backend -d --pull "always" --force-recreate
+docker --context remote compose -f docker-compose.yml up backend -d --pull "always" --force-recreate

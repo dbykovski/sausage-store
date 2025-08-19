71f0b8eb75356a7319a4b7c754d56847cb732730
std-ext-019-01 <dbykovski@yandex.ru>
2025-08-19 22:20:52 +0300

    blue-green

--- backend/deploy-backend.sh
+++ backend/deploy-backend.sh
@@ -10,4 +10,4 @@ fi
 sudo docker login -u ${CI_REGISTRY_USER} -p ${CI_REGISTRY_PASSWORD} ${CI_REGISTRY}
 sudo docker pull ${CI_REGISTRY_IMAGE}/sausage-backend:${VERSION}
 
-docker --context remote compose -f docker-compose-backend-blue.yml up backend -d --force-recreate
+docker --context remote compose -f docker-compose-backend.yml up backend -d --force-recreate

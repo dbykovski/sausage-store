e2400243da0a7e6719f9871d33980c13619c4a11
std-ext-019-01 <std-ext-019-01>
2025-08-19 18:16:21 +0300

    blue-green

--- frontend/deploy-frontend.sh
+++ frontend/deploy-frontend.sh
@@ -10,4 +10,4 @@ fi
 sudo docker login -u ${CI_REGISTRY_USER} -p ${CI_REGISTRY_PASSWORD} ${CI_REGISTRY}
 sudo docker pull ${CI_REGISTRY_IMAGE}/sausage-frontend:${VERSION}
 
-docker --context remote compose -f docker-compose-frontend.yml up frontend -d --pull "always" --force-recreate
+docker --context remote compose -f docker-compose-frontend.yml up frontend -d --force-recreate

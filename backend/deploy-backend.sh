f6f4983d140c176916b7e8e4d1661ab33b652c28
std-ext-019-01 <std-ext-019-01>
2025-08-19 16:55:59 +0300

    blue-green

--- backend/deploy-backend.sh
+++ backend/deploy-backend.sh
@@ -5,4 +5,4 @@ docker context create remote --description "remote ssh" --docker "host=ssh://${D
 sudo docker login -u ${CI_REGISTRY_USER} -p ${CI_REGISTRY_PASSWORD} ${CI_REGISTRY}
 sudo docker pull ${CI_REGISTRY_IMAGE}/sausage-backend:${VERSION}
 
-docker --context remote compose --env-file deploy.env up backend -d --pull "always" --force-recreate
+docker --context remote compose up backend -d --pull "always" --force-recreate

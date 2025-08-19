6e6ec11af681d132c719266a68cacee4666ec05a
std-ext-019-01 <dbykovski@yandex.ru>
2025-08-19 22:46:20 +0300

    blue-green

--- backend/deploy-backend.sh
+++ backend/deploy-backend.sh
@@ -11,4 +11,4 @@ fi
 sudo docker login -u ${CI_REGISTRY_USER} -p ${CI_REGISTRY_PASSWORD} ${CI_REGISTRY}
 sudo docker pull ${CI_REGISTRY_IMAGE}/sausage-backend:${VERSION}
 
-docker --context remote run --rm -v "$PWD:/app" -w /app \ docker:24.0.7-cli \ ./blue-green.sh "${VERSION}"
+docker --context remote run --rm -v "$PWD:/app" -w /app docker:24.0.7-cli ./blue-green.sh "${VERSION}"

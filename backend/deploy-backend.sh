27ffe8fcfed09c7f91ce1a2879f51c30a7f9f248
std-ext-019-01 <dbykovski@yandex.ru>
2025-08-19 23:11:16 +0300

    blue-green

--- backend/deploy-backend.sh
+++ backend/deploy-backend.sh
@@ -1,14 +1,7 @@
 #! /bin/bash
 set -xe
 
-# создаём remote context, если ещё не создан
-if ! docker context inspect remote >/dev/null 2>&1; then
-  docker context create remote \
-    --description 'remote ssh' \
-    --docker host=ssh://student@std-ext-019-01.praktikum-services.tech
-fi
-
 sudo docker login -u ${CI_REGISTRY_USER} -p ${CI_REGISTRY_PASSWORD} ${CI_REGISTRY}
 sudo docker pull ${CI_REGISTRY_IMAGE}/sausage-backend:${VERSION}
 
-docker --context remote run --rm -v "$PWD:/app" -w /app docker:24.0.7-cli ./blue-green.sh "${VERSION}"
+"VERSION=${VERSION} ~/blue-green.sh"
--- frontend/deploy-frontend.sh
+++ frontend/deploy-frontend.sh
@@ -1,12 +1,6 @@
 #! /bin/bash
 set -xe
 
-if ! docker context inspect remote >/dev/null 2>&1; then
-  docker context create remote \
-    --description 'remote ssh' \
-    --docker host=ssh://student@std-ext-019-01.praktikum-services.tech
-fi
-
 sudo docker login -u ${CI_REGISTRY_USER} -p ${CI_REGISTRY_PASSWORD} ${CI_REGISTRY}
 sudo docker pull ${CI_REGISTRY_IMAGE}/sausage-frontend:${VERSION}
 

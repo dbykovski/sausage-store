0a362ce67c9c14637e09ff25b868ca8732b8b8e9
std-ext-019-01 <std-ext-019-01>
2025-08-19 16:43:00 +0300

    blue-green

--- backend/deploy-backend.sh
+++ backend/deploy-backend.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 set -xe
 
-docker context create remote --description "remote ssh" --docker
+docker context create remote --description "remote ssh" --docker "host=ssh://${DEV_USER}@${DEV_HOST}"
 sudo docker login -u ${CI_REGISTRY_USER} -p ${CI_REGISTRY_PASSWORD} ${CI_REGISTRY}
 sudo docker pull ${CI_REGISTRY_IMAGE}/sausage-backend:${VERSION}
 

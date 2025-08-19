08a21798e12311837e6c577bc4bf87707d970ecc
std-ext-019-01 <dbykovski@yandex.ru>
2025-08-19 23:47:43 +0300

    blue-green

--- backend/deploy-backend.sh
+++ backend/deploy-backend.sh
@@ -4,4 +4,4 @@ set -xe
 sudo docker login -u ${CI_REGISTRY_USER} -p ${CI_REGISTRY_PASSWORD} ${CI_REGISTRY}
 sudo docker pull ${CI_REGISTRY_IMAGE}/sausage-backend:${VERSION}
 
-"VERSION=${VERSION} blue-green.sh"
+"VERSION=${VERSION} ./blue-green.sh"
